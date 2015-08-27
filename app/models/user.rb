2396e10d56d54680f4967d18908647e4452dfb28
claucece <soficeli0@gmail.com>
2015-08-27 00:18:25 -0500

    added braintree

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :purchases, foreign_key: :buyer_id
+  has_many :movies, through: :purchases
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,6 +8,31 @@ class User < ActiveRecord::Base
 
   def cart_count
     $redis.scard "cart#{id}"
+  end
+
+  def cart_total_price
+    total_price = 0
+    get_cart_movies.each { |movie| total_price+= movie.price }
+    total_price
+  end
+
+  def get_cart_movies
+    cart_ids = $redis.smembers "cart#{id}"
+    Movie.find(cart_ids)
+  end
+
+  # Purchasing methods
+  def purchase_cart_movies!
+    get_cart_movies.each { |movie| purchase(movie) }
+    $redis.del "cart#{id}"
+  end
+
+  def purchase?(movie)
+    movies.include?(movie)
+  end
+
+  def purchase(movie)
+    movies << movie unless purchase?(movie)
   end
 end
 
